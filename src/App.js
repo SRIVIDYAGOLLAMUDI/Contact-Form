5141c0a492f4a791e29dbbb1e1d7bbb66a9f5693
SRIVIDYAGOLLAMUDI <110527766+SRIVIDYAGOLLAMUDI@users.noreply.github.com>
2025-04-23 15:42:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactForm from "./Component/ContactForm"; 
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
